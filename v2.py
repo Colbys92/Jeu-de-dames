9002ea21cefa46ec20267b7194db136e058b36f2
Colbys92 <matthieu.moreau@eleves.enpc.fr>
2018-02-04 19:04:58 +0100

    ajout du menu

--- v2.py
+++ v2.py
@@ -3,6 +3,7 @@
 #sys.path.append('c:\\users\\matthieu\\documents\\ponts\\2A\\td log\\projet')
 #sys.path.append('D:\\ENPC\\Semestre 3\\ProjetMOPSI\\Projet\\Jeu-de-dames')
 
+import sys
 import datetime
 import sqlite3
 from arbre import *
@@ -63,19 +64,88 @@ class PyBoard(Board):
                 highlightKing(pos,currentPiece.Color(),window)
         
     def display(self,window):
-        # pygame.draw.rect(window,(255,255,255),Rect(262,109,500,500),1)
-        # for i in range(10):
-        #     for j in range(10):
-        #         if (i+j)%2==0:
-        #             pygame.draw.rect(window,(255,255,255),Rect(262+i*50,109+j*50,50,50))
-        #         else:
-        #             pygame.draw.rect(window,(0,0,0),Rect(262+i*50,109+j*50,50,50))
-        # pygame.draw.rect(window,(255,255,255),Rect(262,109,500,500),1)
         for i in range(self.nbPieces()):
             if(self.getPiece(i).isMan()):
                 drawMan(self.getPiece(i),window)
             else:
                 drawKing(self.getPiece(i),window)
+
+class MainMenu():
+    buttonWidth=610
+    buttonHeight=155
+    buttonAbs=207
+    buttonOrd1=72
+    buttonOrd2=310
+    buttonOrd3=542
+    def __init__(self):
+        self.state=1 #1 for gameMode selection, 2 for number of players selection, 3 for difficulty
+        self.window=pygame.display.set_mode((1024,768))
+        self.textureMenu1=pygame.image.load("textures/menu/menu.png").convert_alpha()
+        self.textureMenu2=pygame.image.load("textures/menu/menu_1.1.png").convert_alpha()
+        self.textureMenu3=pygame.image.load("textures/menu/menu_1.1.2.png").convert_alpha()
+    def run(self):
+        choiceMade=False
+        while(not choiceMade):
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type==KEYDOWN and event.key==K_ESCAPE and self.state>1:
+                    self.state-=1
+                if event.type == MOUSEBUTTONDOWN and event.button==1:
+                    (x,y)=pygame.mouse.get_pos()
+                    if coordInRect((x,y),(self.buttonAbs,self.buttonOrd1,self.buttonWidth,self.buttonHeight)):
+                        print(1)
+                        if(self.state==3):
+                            #learningMode
+                            choiceMade=True
+                        else:
+                            self.state+=1
+                    elif coordInRect((x,y),(self.buttonAbs,self.buttonOrd2,self.buttonWidth,self.buttonHeight)):
+                        choiceMade=True
+                        print(2)
+                        if(self.state==1):
+                            choiceMade=True
+                            #replay
+                        elif(self.state==2):
+                            choiceMade=True
+                            #player vs player
+                        else:
+                            choiceMade=True
+                            #difficulty moyen
+                    elif coordInRect((x,y),(self.buttonAbs,self.buttonOrd3,self.buttonWidth,self.buttonHeight)):
+                        choiceMade=True
+                        if(self.state==1):
+                            pygame.quit()
+                        elif(self.state==2):
+                            choiceMade=True
+                            #IA vs IA
+                        else:
+                            choiceMade=True
+                            #difficile
+            self.display()
+    def display(self):
+        if(self.state==1):
+            self.window.blit(self.textureMenu1,(0,0))
+        elif(self.state==2):
+            self.window.blit(self.textureMenu2,(0,0))
+        else:
+            self.window.blit(self.textureMenu3,(0,0))
+        pygame.display.flip()
+ 
+ 
+class GameWindow(Board):
+    def __init__(self):
+        a=0
+ 
+ 
+ 
+ 
+ 
+def coordInRect(pos,rect):
+    #check whether pos is within the rectangle rect
+    return not (pos[0]<rect[0] or pos[0]>rect[0]+rect[2] or pos[1]<rect[1] or pos[1]>rect[1]+rect[3])
+
 
 def coordToPos(x,y):
     i=(x-262)//50
@@ -134,14 +204,30 @@ def loadMoveList(gameId):
         #on utilise la fonction eval... Qui peut conduire à des problèmes de sécurité.. ON s'autorise à le faire vu le cadre du projet.
         conn.close()
         return eval(row[0])
-        #print(row)
-    
 
+def updateGraphSize(valueList,ax):
+    if ax.get_ylim()[0]>listeValeurs[-1]:
+            ax.set_ylim(ax.get_ylim()[0]-10,ax.get_ylim()[1])
+    if ax.get_ylim()[1]<listeValeurs[-1]:
+        ax.set_ylim(ax.get_ylim()[0],ax.get_ylim()[1]+10)
+    if ax.get_xlim()[1]<len(listeValeurs):
+        ax.set_xlim(0,ax.get_xlim()[1]+10)
 
-
-
+def drawGraph(window,fig):
+    canvas=agg.FigureCanvasAgg(fig)
+    canvas.draw()
+    renderer=canvas.get_renderer()
+    raw_data=renderer.tostring_rgb()
+    size=canvas.get_width_height()
+    surf=pygame.image.fromstring(raw_data,size,"RGB")
+    screen.blit(surf,(1024,184))
+    pygame.display.flip()
 
 if __name__ == "__main__":
+    pygame.init()
+    menu=MainMenu()
+    menu.run()
+    
     #on charge la base données
     conn=sqlite3.connect('example.db')
     c=conn.cursor()
@@ -167,7 +253,7 @@ if __name__ == "__main__":
     difficulty=1
     
 
-    
+   #   
     fond=pygame.image.load("textures/surface_jeu_V1.png").convert()
     window.blit(fond,(0,0))
     plateau=PyBoard()
@@ -176,6 +262,8 @@ if __name__ == "__main__":
     
     fig = pylab.figure(figsize=[4, 4], dpi=100,)
     ax = fig.gca()
+    ax.set_xlim(0,20)
+    ax.set_ylim(-20,20)
     
     
     #initialisation variable de calcul
@@ -187,7 +275,7 @@ if __name__ == "__main__":
     listeValeurs=[]
     chosenPiece=-1
 
-    window.blit(fond,(0,0))
+   #   window.blit(fond,(0,0))
     plateau.display(window)
     pygame.display.flip()
     
@@ -249,23 +337,15 @@ if __name__ == "__main__":
                             pygame.display.flip()
                             if(gameType==4):
                                 listeValeurs.append(plateau.evaluateBetter(1,5,0.1,0.3,0.2,"white"))
-                            move=Test(plateau.bestMoveAlphaBeta2(couleurs[1-compteur],4,5,10,0,0,0,True,-10000,10000))
+                            move=Test(plateau.bestMoveAlphaBeta2(couleurs[1-compteur],6,5,10,0,0,0,True,-10000,10000))
                             listeCoups.append(([move.getPath()[i] for i in range(len(move.getPath()))],move.getKills()))
                             plateau.playMove(move,False)
                             moves=plateau.playableMoves(couleurs[compteur])
                         chosenPiece=-1
             if(gameType==4 and len(listeValeurs)>1):
                 ax.plot(listeValeurs,'b-')
-                ax.set_xlim(0,20)
-                ax.set_ylim(-20,20)
-                canvas=agg.FigureCanvasAgg(fig)
-                canvas.draw()
-                renderer=canvas.get_renderer()
-                raw_data=renderer.tostring_rgb()
-                size=canvas.get_width_height()
-                surf=pygame.image.fromstring(raw_data,size,"RGB")
-                screen.blit(surf,(1024,184))
-                pygame.display.flip()
+                updateGraphSize(listeValeurs,ax)
+                drawGraph(window,fig)
         stringMove=listeCoups.__str__()
         stringDate=datetime.date.today().__str__()
         c.execute("INSERT INTO games VALUES (10,?,?)",(stringDate,stringMove))
@@ -304,7 +384,7 @@ if __name__ == "__main__":
             pygame.display.flip()
             pygame.time.delay(waitingTime)
 
-    ##   pygame.quit()
+    #   pygame.quit()
 
 
 
